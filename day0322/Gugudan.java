44352df1cb93a32b0596f5ec0e259c44c5120384
SYoungBird <101791448+SYoungBird@users.noreply.github.com>
2022-03-22 14:38:04 +0900

    Add files via upload

---
+++ day0322/Gugudan.java
@@ -0,0 +1,16 @@
+package day0322;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("구구단 3단 출력");
+		int multip3 = 1;
+		for(multip3=1; multip3<=9; multip3++)
+		{
+			System.out.println("3*"+multip3+"="+3*multip3);
+		}
+
+	}
+
+}
