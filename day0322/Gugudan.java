013fb67fdf138de50549ba7912e270302b2b6b0b
SYoungBird <101791448+SYoungBird@users.noreply.github.com>
2022-03-22 14:48:03 +0900

    Add files via upload

--- day0322/Gugudan.java
+++ day0322/Gugudan.java
@@ -1,15 +1,24 @@
 package day0322;
 
+import java.util.Scanner;
+
 public class Gugudan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("구구단 3단 출력");
-		int multip3 = 1;
-		for(multip3=1; multip3<=9; multip3++)
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("구구단 몇단을 외우고 싶나요?");
+		int gugudan = sc.nextInt();
+		int multip = 0;
+		int ans = 0;
+		
+		for(multip=1; multip <=9; multip++)
 		{
-			System.out.println("3*"+multip3+"="+3*multip3);
+			ans=gugudan*multip;
+			System.out.println(gugudan+"*"+multip+"="+ans);
 		}
+		
 
 	}
 
