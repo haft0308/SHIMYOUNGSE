5b5316844ce1cda9606884787e6861e7a7dc1709
SYoungBird <101791448+SYoungBird@users.noreply.github.com>
2022-03-23 15:20:47 +0900

    Add files via upload

---
+++ homework01/Homework0323_01.java
@@ -0,0 +1,16 @@
+package day0323;
+
+public class Homework0323_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] score = new int[5];
+		for(int i=0; i<5; i++)
+		{
+			score[i]=10*i;
+			System.out.println("score"+"["+i+"] = "+score[i]);
+		}
+	}
+
+}
---
+++ homework01/Homework0323_02.java
@@ -0,0 +1,59 @@
+package day0323;
+
+public class Homework0323_02 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte[] arrByte = new byte[5];
+		arrByte[0] = 1;
+		arrByte[1] = 2;
+		arrByte[2] = 3;
+		arrByte[3] = 4;
+		arrByte[4] = 5;
+		
+		char[] arrChar = new char[5];
+		arrChar[0] = 'A';
+		arrChar[1] = 'B';
+		arrChar[2] = 'C';
+		arrChar[3] = 'D';
+		arrChar[4] = 'E';
+		
+		boolean[] arrBoolean = new boolean[5];
+		arrBoolean[0] = false;
+		arrBoolean[1] = true;
+		arrBoolean[2] = false;
+		arrBoolean[3] =	true;
+		arrBoolean[4] = false;
+		
+		short[] arrShort = new short[5];
+		arrShort[0] = 100;
+		arrShort[1] = 200;
+		arrShort[2] = 300;
+		arrShort[3] = 400;
+		arrShort[4] = 500;
+		
+		long[] arrLong = new long[5];
+		arrLong[0] = 90;
+		arrLong[1] = 135;
+		arrLong[2] = 180;
+		arrLong[3] = 225;
+		arrLong[4] = 270;
+		
+		float[] arrFloat = new float[5];
+		arrFloat[0] = 3.14f;
+		arrFloat[1] = 6.28f;
+		arrFloat[2] = 9.42f;
+		arrFloat[3] = 12.56f;
+		arrFloat[4] = 15.700001f;
+		
+		double[] arrDouble = new double[5];
+		arrDouble[0] = 3.141592;
+		arrDouble[1] = 3.141592;
+		arrDouble[2] = 3.141592;
+		arrDouble[3] = 3.141592;
+		arrDouble[4] = 3.141592;
+		
+		
+	}
+
+}
